585235db6c7b73415b07a8737b5e1da80fdd9654
kajotello <kajotello@gmail.com>
2024-05-14 22:51:17 +0200

    add type hints in vertex adjustment

--- src/vertex_adjustment.py
+++ src/vertex_adjustment.py
@@ -2,6 +2,7 @@ from numpy.linalg import lstsq
 import numpy as np
 from shapely.geometry import Point, Polygon
 from shapely.ops import nearest_points
+from typing import Tuple
 
 
 class _Curve:
@@ -30,14 +31,20 @@ class _Segment:
         self.beta = 0.0
 
 
-def fit_straight_line(points):
+def fit_least_squares(points: list[int]):
+
+    """Fit linear function to given points in 2D with least squares method"""
+
     x = np.array([point[0] for point in points])
     y = np.array([point[1] for point in points])
     A = np.vstack([x, np.ones(len(x))]).T
     return lstsq(A, y, rcond=None)[0]
 
 
-def calculate_intersection_point(first_parameters, second_parameters):
+def calculate_intersection_point(first_parameters: Tuple[int, int], second_parameters: Tuple[int, int]):
+
+    """Calculate point of intersection for two linear functions"""
+
     a, b = first_parameters
     c, d = second_parameters
     x_intersection = (d-b)/(a-c)
@@ -45,7 +52,13 @@ def calculate_intersection_point(first_parameters, second_parameters):
     return (x_intersection, y_intersection)
 
 
-def find_closest_point_in_boundary(original_point, boundary_center, boundary_manhatan_range):
+def find_closest_point_in_boundary(original_point: Tuple[int, int],
+                                   boundary_center: Tuple[int, int],
+                                   boundary_manhatan_range: float
+                                   ):
+
+    """Find point closest to original point but lying inside the square boundary around another point"""
+
     boundary_center_x, boundary_center_y = boundary_center
     poly = Polygon([(boundary_center_x+boundary_manhatan_range, boundary_center_y+boundary_manhatan_range),
                     (boundary_center_x-boundary_manhatan_range, boundary_center_y+boundary_manhatan_range),
@@ -56,33 +69,32 @@ def find_closest_point_in_boundary(original_point, boundary_center, boundary_man
     return p1
 
 
-def adjust_vertices(path, polygon_points) -> int:
+def adjust_vertices(path: list[(int, int)], polygon_points_idxs: list[int]) -> _Curve:
     """
-    /* Adjust vertices of optimal polygon: calculate the intersection of
+    Adjust vertices of optimal polygon: calculate the intersection of
      the two "optimal" line segments, then move it into the unit square
-     if it lies outside. Return 1 with errno set on error; 0 on
-     success. */
+     if it lies outside.
     """
-    curve = _Curve(len(polygon_points))
+    curve = _Curve(len(polygon_points_idxs))
 
-    points = path[polygon_points[-2]:polygon_points[-1]+1]
-    prev_coeff = fit_straight_line(points)
+    points = path[polygon_points_idxs[-2]:polygon_points_idxs[-1]+1]
+    prev_coeff = fit_least_squares(points)
 
-    points = path[polygon_points[-1]:] + [path[polygon_points[0]]]
-    coeffs = fit_straight_line(points)
+    points = path[polygon_points_idxs[-1]:] + [path[polygon_points_idxs[0]]]
+    coeffs = fit_least_squares(points)
 
     intersection_point = calculate_intersection_point(coeffs, prev_coeff)
-    point_in_boundaries = find_closest_point_in_boundary(intersection_point, path[polygon_points[-1]], 0.5)
+    point_in_boundaries = find_closest_point_in_boundary(intersection_point, path[polygon_points_idxs[-1]], 0.5)
 
     curve[-1].vertex[0] = point_in_boundaries.x
     curve[-1].vertex[1] = point_in_boundaries.y
-    prev_point_idx = polygon_points[0]
+    prev_point_idx = polygon_points_idxs[0]
     prev_coeff = coeffs
 
-    for i, polygon_point in enumerate(polygon_points[1:]):
+    for i, polygon_point in enumerate(polygon_points_idxs[1:]):
 
         points = path[prev_point_idx:polygon_point+1]
-        coeffs = fit_straight_line(points)
+        coeffs = fit_least_squares(points)
 
         intersection_point = calculate_intersection_point(coeffs, prev_coeff)
         point_in_boundaries = find_closest_point_in_boundary(intersection_point, path[polygon_point], 0.5)
